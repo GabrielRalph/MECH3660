b9f3565504384f63ddc4e09376a1523cc3400ddc
GabrielRalph <gltralph@gmail.com>
2020-05-21 17:49:37 +1000

    open

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ var list = ['a1parta'];
 var html = ''
 
 for(var i in list){
-  html += `<h1 onclick = "window.open('mech3600.galetora.com/${list[i]}')">${list[i]}</h1>\n`
+  html += `<h1 onclick = "window.open('${list[i]}/')">${list[i]}</h1>\n`
 }
 document.getElementById('app').innerHTML += html
