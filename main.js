0aaa2ec16106ea571c3e034d226231a7a75e7d04
GabrielRalph <gltralph@gmail.com>
2020-05-21 18:02:29 +1000

    one site

--- main.js
+++
@@ -1,7 +0,0 @@
-var list = ['a1parta'];
-var html = ''
-
-for(var i in list){
-  html += `<h1 onclick = "window.open('${list[i]}/')">${list[i]}</h1>\n`
-}
-document.getElementById('app').innerHTML += html
